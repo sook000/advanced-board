073386a1ab85fef115d14d7af6f978c5d57fe95b
안치숙 <songweddin@naver.com>
2025-06-18 02:20:46 +0900

    Init: 프로젝트 생성

---
+++ BE/src/main/java/com/example/advanced_board/AdvancedBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.advanced_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedBoardApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/example/advanced_board/AdvancedBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advanced_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
