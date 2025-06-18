7d557eb82f5af61629baf0faf6ba646050897a39
안치숙 <songweddin@naver.com>
2025-06-18 16:38:00 +0900

    Feat: 데이터 120만건 삽입

---
+++ BE/service/article/src/test/java/advanced/board/article/api/ArticleApiTest.java
@@ -0,0 +1,80 @@
+package advanced.board.article.api;
+
+
+import advanced.board.article.service.response.ArticleResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class ArticleApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest() {
+        ArticleResponse response = create(new ArticleCreateRequest(
+                "hi", "my conent", 1L, 1L
+        ));
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse create(ArticleCreateRequest request) {
+        return restClient.post()
+                .uri("/v1/articles")
+                .body(request)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void readTest() {
+        ArticleResponse response = read(193607157362552832L);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse read(Long articleId) {
+        return restClient.get()
+                .uri("/v1/articles/{articleId}", articleId)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void updateTest() {
+        update(193607157362552832L);
+        ArticleResponse response = read(193607157362552832L);
+        System.out.println("response = " + response);
+    }
+
+    void update(Long articleId) {
+        restClient.put()
+                .uri("/v1/articles/{articleId}", articleId)
+                .body(new ArticleUpdateRequest("hi 2", "my content 22"))
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    @Test
+    void deleteTest() {
+        restClient.delete()
+                .uri("/v1/articles/{articleId}", 193607157362552832L)
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleCreateRequest {
+        private String title;
+        private String content;
+        private Long writerId;
+        private Long boardId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleUpdateRequest {
+        private String title;
+        private String content;
+    }
+}
