21e4987063306e6e9d20a66e4f17d72352652246
안치숙 <songweddin@naver.com>
2025-06-18 16:37:50 +0900

    Feat: 게시판 기본 CRUD 구현

---
+++ BE/service/article/src/main/java/advanced/board/article/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package advanced.board.article.repository;
+
+import advanced.board.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
