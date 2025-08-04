e08ee178db581d9df1eacc0c1857a49e84e15f4b
안치숙 <songweddin@naver.com>
2025-08-04 19:30:10 +0900

    Feat: 댓글 무한 depth로 crud 기능 개발

--- BE/service/comment/src/main/java/advanced/board/comment/service/response/CommentResponse.java
+++ BE/service/comment/src/main/java/advanced/board/comment/service/response/CommentResponse.java
@@ -1,6 +1,7 @@
 package advanced.board.comment.service.response;
 
 import advanced.board.comment.entity.Comment;
+import advanced.board.comment.entity.CommentV2;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -23,6 +24,18 @@ public class CommentResponse {
         response.commentId = comment.getCommentId();
         response.content = comment.getContent();
         response.parentCommentId = comment.getParentCommentId();
+        response.articleId = comment.getArticleId();
+        response.writerId = comment.getWriterId();
+        response.deleted = comment.getDeleted();
+        response.createdAt = comment.getCreatedAt();
+        return response;
+    }
+
+    public static CommentResponse from(CommentV2 comment) {
+        CommentResponse response = new CommentResponse();
+        response.commentId = comment.getCommentId();
+        response.content = comment.getContent();
+        response.path = comment.getCommentPath().getPath();
         response.articleId = comment.getArticleId();
         response.writerId = comment.getWriterId();
         response.deleted = comment.getDeleted();
