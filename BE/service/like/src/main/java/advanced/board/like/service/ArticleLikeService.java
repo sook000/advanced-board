89c6678ead1843659beb03d67cdece7d2ec51eaa
안치숙 <songweddin@naver.com>
2025-08-05 07:44:24 +0900

    Feat: 좋아요 crud 기능 개발

---
+++ BE/service/like/src/main/java/advanced/board/like/service/ArticleLikeService.java
@@ -0,0 +1,40 @@
+package advanced.board.like.service;
+
+import advanced.board.common.snowflake.Snowflake;
+import advanced.board.like.entity.ArticleLike;
+import advanced.board.like.repository.ArticleLikeRepository;
+import advanced.board.like.service.response.ArticleLikeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleLikeService {
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleLikeRepository articleLikeRepository;
+
+    public ArticleLikeResponse read(Long articleId, Long userId) {
+        return articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .map(ArticleLikeResponse::from)
+                .orElseThrow();
+    }
+
+    @Transactional
+    public void like(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+    }
+
+    @Transactional
+    public void unlike(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLikeRepository::delete);
+    }
+
+}
