a08f08e254af22e0a79123cb826b4453049961a2
안치숙 <songweddin@naver.com>
2025-08-05 19:45:06 +0900

    Feat: 조회 수 조회
    Redis를 이용한 분산락
    batch로 데이터 백업

---
+++ BE/service/view/src/main/java/advanced/board/view/service/ArticleViewService.java
@@ -0,0 +1,35 @@
+package advanced.board.view.service;
+
+import advanced.board.view.repository.ArticleViewCountRepository;
+import advanced.board.view.repository.ArticleViewDistributedLockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleViewService {
+    private final ArticleViewCountRepository articleViewCountRepository;
+    private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+
+    private static final int BACK_UP_BACH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
+
+    public Long increase(Long articleId, long userId) {
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
+        Long count = articleViewCountRepository.increase(articleId);
+        if (count % BACK_UP_BACH_SIZE == 0) {
+            articleViewCountBackUpProcessor.backUp(articleId, count);
+        }
+        return count;
+    }
+
+    public Long count(Long articleId) {
+        return articleViewCountRepository.read(articleId);
+    }
+}
---
+++ BE/service/view/src/test/java/advanced/board/view/repository/ArticleViewCountBackUpRepositoryTest.java
@@ -0,0 +1,44 @@
+package advanced.board.view.repository;
+
+import advanced.board.view.entity.ArticleViewCount;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ArticleViewCountBackUpRepositoryTest {
+    @Autowired
+    ArticleViewCountBackUpRepository articleViewCountBackUpRepository;
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Test
+    @Transactional
+    void updateViewCountTest() {
+        // given
+        articleViewCountBackUpRepository.save(
+                ArticleViewCount.init(1L, 0L)
+        );
+        entityManager.flush();
+        entityManager.clear();
+
+        // when
+        int result1 = articleViewCountBackUpRepository.updateViewCount(1L, 100L);
+        int result2 = articleViewCountBackUpRepository.updateViewCount(1L, 300L);
+        int result3 = articleViewCountBackUpRepository.updateViewCount(1L, 200L);
+
+        // then
+        assertThat(result1).isEqualTo(1);
+        assertThat(result2).isEqualTo(1);
+        assertThat(result3).isEqualTo(0);
+
+        ArticleViewCount articleViewCount = articleViewCountBackUpRepository.findById(1L).get();
+        assertThat(articleViewCount.getViewCount()).isEqualTo(300L);
+    }
+}
